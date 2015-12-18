8fe2bb09ba71d3e5b7a728d4ff488a64d38c8e4f
elessert760 <elessert760@users.noreply.github.com>
2015-12-18 14:39:02 -0800

    Update fill values down.R

--- fill values down.R
+++ fill values down.R
@@ -1,27 +1,27 @@
-# Assume you have a data frame named houston and that if CCN is empty then the record is meant to be
-# assigned to the instance above where CCN is filled in
+# Assume you have a data frame named df and that if "column" is empty then the record is meant to be
+# assigned to the instance above where "column" is filled in
 
-for (p in colnames(houston)) {
-  houston[,p] <- houston[, p] %>% trimws()
+for (p in colnames(df)) {
+  df[,p] <- df[, p] %>% trimws()
 }
 
-houston <- houston %>% unique()
+df <- df %>% unique()
 
 
-change <- which(houston[,"CCN"] != "")
+change <- which(df[,"column"] != "")
 
-for (p in colnames(houston)) {
+for (p in colnames(df)) {
   for (i in change) {
-    if (houston[i,p] == "" | is.na(houston[i,p])) {
-      houston[i, p] <- "Not Provided"
+    if (df[i,p] == "" | is.na(df[i,p])) {
+      df[i, p] <- "Not Provided"
     }
   }
 }
 
-for (j in colnames(houston)) {
-  for (i in 1:nrow(houston)) {
-    if (houston[i, j] == "" | is.na(houston[i,j])) {
-      houston[i, j] <- houston[i - 1 , j]
+for (j in colnames(df)) {
+  for (i in 1:nrow(df)) {
+    if (df[i, j] == "" | is.na(df[i,j])) {
+      df[i, j] <- df[i - 1 , j]
     }
   }
 }
